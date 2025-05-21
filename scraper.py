2758740a86ee03ad9ee47687c0cb597bd9be0b18
Elijah Appelson <124807267+appelson@users.noreply.github.com>
2025-05-20 23:26:49 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,22 +1,46 @@
-import requests
-from bs4 import BeautifulSoup
-import re
+# Importing packages
+import os
 import time
-import pandas as pd
- 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
+# Base URL
+url = 'https://www.dla.mil/Disposition-Services/Offers/Law-Enforcement/Public-Information/'
 
-source_code = requests.get("https://www.law.umich.edu/special/exoneration/Pages/detaillist.aspx")
-soup = BeautifulSoup(source_code.content, 'lxml')
+# Creating a download directory
+download_dir = os.path.join(os.getcwd(), 'downloads')
+os.makedirs(download_dir, exist_ok=True)
 
-link_list = []
-for link in soup.findAll('a'):
-  new_link = link.get('href')
-  link_list.append(new_link)
+# Setting up headless Chromium driver with necessary options
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_experimental_option('prefs', {
+    'download.default_directory': download_dir,
+    'download.prompt_for_download': False,
+    'directory_upgrade': True
+})
 
-pruned_link_list = ["https://www.law.umich.edu/special/exoneration/Pages/" + value for value in link_list if 'casedetail' in value]
-df = pd.DataFrame(pruned_link_list)
+# Initializing the WebDriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-df.to_csv("exhoneration_links.csv")
+# Opening the page and parsing content
+driver.get(url)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+# Finding all Excel file links
+xlsx_links = [link['href'] for link in soup.find_all('a', href=True) if '.xlsx' in link['href']]
 
+# Downloading Excel files
+for link in xlsx_links:
+    driver.get("https://www.dla.mil" + link)
+    while any(file.endswith('.crdownload') for file in os.listdir(download_dir)):
+        time.sleep(3)
+    print(f"Download completed: {'https://www.dla.mil'+link}")
+
+# Closing the driver
+driver.quit()
